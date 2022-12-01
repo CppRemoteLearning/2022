4f125478edab5f0cea5c89e90d4fc5ee1bd30eda
aronkis <62013400+aronkis@users.noreply.github.com>
2022-12-02 00:34:06 +0200

    Update first_test_header.h

--- include/first_test_header.h
+++ include/first_test_header.h
@@ -1,8 +1,6 @@
 #ifndef AKIS_MAIN_FIRST_TEST_HEADER_H_
 #define AKIS_MAIN_FIRST_TEST_HEADER_H_
 
-#include <string>
-
 int CalculateSumOfNInRange (const int &beg, const int &end);
 double CeliusToFahrenheit(const double &celsius);
 
