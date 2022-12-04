7a5f8443657c75a72bcf3c965f48ad2ef4120dcc
adrian-ilie <oprisoradrianilie@gmail.com>
2022-12-04 18:20:26 +0200

    style: update line limit for main.cpp

--- course_1_homework/main.cpp
+++ course_1_homework/main.cpp
@@ -1,21 +1,10 @@
-
 #include <iostream>
 
 #include "math_function.h"
 
 int main(){
-
-	int a, b;
-	std::cout << "Calculul factorialului" << std::endl;
-	std::cout << "nr. = ";
-	std::cin >> a;
-	std::cout << a << "! = " << factorial_recursiv(a) << "\n\n";
-
-	std::cout << "Calculul c.m.m.d.c-ului" << std::endl; 
-	std::cout << "a = ";
-	std::cin >> a;
-	std::cout << "b = ";
-	std::cin >> b;
+	int a=10, b=12;
+	std::cout << a << "! = " << factorial_recursiv(a) << std::endl;
 	std::cout << "c.m.m.d.c dintre " << a << " si " << b << " este " << euclide_recursiv(a,b) << std::endl;
 	return 0;
 }
