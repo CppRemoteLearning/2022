bddee302e4028717f928999f091137764219c412
Voicuu <andrei.voicu2@student.upt.ro>
2022-12-05 21:56:12 +0200

    yes v2

--- tema1/main.cpp
+++ tema1/main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 #include "functii.h"
-
 int main()
 {
     int a, b, x;
-
-    cout << "a = ";
-    cin >> a;
-
-    cout << "b = ";
-    cin >> b;
-
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
     cout << "Suma este: " << suma(a, b) << endl;
-
-    cout << "Introduceti numarul pe care doriti sa il verificati daca este nr Armstrong: ";
-    cin >> x;
-    armstrong(x);
+    cout << "Introduceti numarul pe care doriti sa il verificati daca este nr Armstrong: "; cin >> x; armstrong(x);
 }
