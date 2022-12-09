4da2b5f6efc1ef2254b18bf2e30bc9b3c05e15b6
andreea-bumbac <andreeabumbac2002@gmail.com>
2022-12-09 18:19:58 +0200

    tema1

---
+++ CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/file1.cpp.o"
+  "CMakeFiles/project1.dir/file2.cpp.o"
+  "CMakeFiles/project1.dir/file3.cpp.o"
+  "project1"
+  "project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
