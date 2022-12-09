d064f1b1f4d0c4ccf9d2043f77bb9a19567fdc20
andreea-bumbac <andreeabumbac2002@gmail.com>
2022-12-09 21:16:32 +0200

    new

--- Header.h
+++ Header.h
@@ -1,7 +1,6 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
 #include <iostream>
-using namespace std;
 
 
 int suma(int a, int b);
--- file3.cpp
+++ file3.cpp
@@ -4,5 +4,5 @@ int main() {
 
     int nr1=2, nr2=3, rezultat;
     rezultat = patrat(nr1, nr2);
-    cout << rezultat << "\n";
+    std::cout << rezultat << "\n";
 }
